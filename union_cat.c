bb28d17f7eeaba271115827752fde1d5d1b4ef78
Mohamed Taha Raouf <moraouf@e1r4p13.1337.ma>
2024-09-15 21:39:00 +0100

    add a simple code of union

---
+++ union_cat.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+
+#include <stdio.h>
+
+char *ft_strcat(char *dest,char *src)
+{
+	int i = 0;
+	int j = 0;
+	while(dest[j])
+	{
+		j++;
+	}
+	while(src[i])
+	{
+		dest[j++] = src[i++];
+	}
+	dest[j] = '\0';
+	return(dest);
+}
+int main(int ac ,char **av)
+{
+	int i;
+	int j;
+	int l;
+	if(ac == 3)
+	{
+		char *s = ft_strcat(av[1],av[2]);
+		i = 0;
+		while(s[i])
+		{
+			j = 0;
+			l = 0;
+			while(i > j)
+			{
+				if(s[i] == s[j])
+				{
+					l = 1;
+				}
+				j++;
+			}
+			if(l == 0)
+			{
+				write(1,&s[i],1);
+			}
+			i++;
+		}
+	}
+	write(1,"\n",1);
+}
